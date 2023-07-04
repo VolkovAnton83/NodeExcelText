cc41fcab63e38243fb44ebd5b75b4ffce46a4f7e
VolkovAnton83 <wsxedc83@mail.ru>
2023-07-05 01:14:14 +0300

    add excel demo creation

---
+++ excelfunc.js
@@ -0,0 +1,57 @@
+//var excel = require('excel4node');
+//ExcelJS for read? 2k packets. 
+
+module.exports = { CreateTestExcelFiles }
+
+function CreateExcelList(wrkbook,wrksheetname) {
+    var worksheet = wrkbook.addWorksheet(wrksheetname);
+    worksheet.cell(1,1).string(Date.now().toString());
+}
+
+function CreateTestExcelFiles(FolderPath) {
+    
+    var workbook = new excel.Workbook();
+    const t0 = performance.now();
+    
+    var curDate = new Date('01/01/2024 00:00:01');
+    while (curDate < new Date('12/31/2024 00:00:01')) {
+        curDate.setDate(curDate.getDate() + 1);
+        if ((curDate.getDay() == 1) || (curDate.getDay() == 3) ) {
+            CreateExcelList(workbook,curDate.toLocaleDateString('ru-RU'));
+        }
+        //console.log('' + curDate.toLocaleDateString('ru-RU') +' - '+ curDate.getDay());
+    }
+    console.log(`Call to doSomething took ${(performance.now() - t0)} milliseconds.`);
+
+
+    // var worksheet = workbook.addWorksheet('Sheet 1');
+    // var worksheet2 = workbook.addWorksheet('Sheet 2');
+   
+    // var style = workbook.createStyle({
+    // font: {
+    //     color: '#FF0800',
+    //     size: 12
+    // },
+    // numberFormat: '$#,##0.00; ($#,##0.00); -'
+    // });
+
+    // worksheet.cell(1,1).number(100).style(style);
+
+    // worksheet.cell(1,2).number(200).style(style);
+
+    // worksheet.cell(1,3).formula('A1 + B1').style(style);
+
+    // worksheet.cell(2,1).string('string').style(style);
+
+    // worksheet.cell(3,1).bool(true).style(style).style({font: {size: 14}});
+
+    //const xmas95 = new Date("1995-12-25T23:15:30");
+    
+    
+    //console.log(seconds); // 30
+
+    // worksheet.cell(1,1).string(Date.now().toString());
+
+    workbook.write(FolderPath+'\\Журнал поступлений на '+curDate.getFullYear()+' год.xlsx');
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var conf = require("config");
+
+var excelfunc = require("./excelfunc.js");
+
+
 if (conf.has("ExcelFilesCatalog")) {
     console.log(conf.get("ExcelFilesCatalog"));
+    excelfunc.CreateTestExcelFiles(conf.get("ExcelFilesCatalog"))
+    
 }else {
     console.log("Not find fucking catalog!");
+    return;
 }
+
+
